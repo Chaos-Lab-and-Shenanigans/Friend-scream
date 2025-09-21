62890b68ebf13010f9f3827ddd5282372507e320
dattebayo <fkgame061@gmail.com>
2025-09-21 09:44:42 +0500

    +refactored +fixed the extra_mock func

--- FriendScream.c
+++ FriendScream.c
@@ -24,52 +24,13 @@ int folder_exists(WCHAR *fullPath) {
     return (attrs & FILE_ATTRIBUTE_DIRECTORY) != 0;
 }
 
-int create_amogus_file(WCHAR *path) {
-    WCHAR full_path[MAX_PATH];
-    swprintf(full_path, MAX_PATH, L"%ls\\amogus.txt", path);
-
-    HANDLE hFile = CreateFileW(
-        full_path,               // use full path here
-        GENERIC_WRITE,
-        0,
-        NULL,
-        CREATE_ALWAYS,
-        FILE_ATTRIBUTE_NORMAL,
-        NULL
-    );
-
-    if (hFile == INVALID_HANDLE_VALUE) {
-        wprintf(L"Error creating file: %lu\n", GetLastError());
-        return 1;
-    }
-
-    const char *data = "Find the imposta!";
-    DWORD bytesWritten;
-    WriteFile(hFile, data, (DWORD)strlen(data), &bytesWritten, NULL);
-
-    CloseHandle(hFile);
-    return 0;
-}
-
-int delete_amogus_file(WCHAR *folderPath) {
-    WCHAR fullPath[MAX_PATH * 4]; // Extended buffer for \\?\ prefix
-    swprintf(fullPath, MAX_PATH, L"%ls\\amogus.txt", folderPath);
-    if (DeleteFileW(fullPath)) {
-        wprintf(L"Deleted: %ls\n", fullPath);
-        return 0; // Success
-    } else {
-        MessageBoxA(NULL, "Could not find Desktop path.", "Error", MB_ICONERROR);
-        exit(1); // Failure
-    }
-}
-
 void create_amogus_folders(int num, WCHAR *path) {
     for (int i = 0; i < num; i++) {
         WCHAR folder_path[MAX_PATH] = L"";
         swprintf(folder_path, MAX_PATH, L"\\\\?\\%ls\\AMOGUS_%d.", path, i + 1);
         if (folder_exists(folder_path))   continue;
         CreateDirectoryW(folder_path, NULL);
-        create_amogus_file(folder_path);
+        //create_amogus_file(folder_path);
         Sleep(delay);
     }
 }
@@ -79,7 +40,7 @@ void delete_amogus_folders(WCHAR *path) {
             WCHAR folder_path[MAX_PATH] = L"";
             swprintf(folder_path, MAX_PATH, L"\\\\?\\%ls\\AMOGUS_%d.", path, i + 1);
             if (!folder_exists(folder_path))  continue;
-            delete_amogus_file(folder_path);
+            //delete_amogus_file(folder_path);
             RemoveDirectoryW(folder_path);
             Sleep(delay);
     }
@@ -88,6 +49,7 @@ void delete_amogus_folders(WCHAR *path) {
 int extra_mock(WCHAR *path) {
     WCHAR full_path[MAX_PATH] = L"";
     swprintf(full_path, MAX_PATH, L"%ls\\Delete-Desktop-Folders.txt", path);
+
     HANDLE hFile = CreateFileW(
         full_path, // extended path
         GENERIC_WRITE,
@@ -99,7 +61,10 @@ int extra_mock(WCHAR *path) {
     );
 
     if (hFile == INVALID_HANDLE_VALUE) {
-        wprintf(L"Error creating file: %lu\n", GetLastError());
+        DWORD err = GetLastError();
+        WCHAR buffer[256] = L"";
+        swprintf(buffer, 256, L"Error creating file. Error: %lu", err);
+        MessageBoxW(NULL, L"Error", buffer, MB_OK | MB_ICONERROR);
         return 1;
     }
     char text[1000] = "Bro\nYou're totally fked\n";
@@ -177,10 +142,8 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrev, LPSTR lpCmdLine, int nCmdSh
         char msg[500] = "";
         sprintf(msg, "Good luck deleting those %d folders on Desktop!\nYou're welcome.", lucky);
         MessageBoxA(NULL, msg, "Done", MB_OK | MB_ICONINFORMATION);
+        extra_mock(desktop);
         create_amogus_folders(lucky, desktop);
-        WCHAR current_dir[MAX_PATH];
-        GetCurrentDirectoryW(MAX_PATH, current_dir);
-        extra_mock(current_dir);
     }
     return 0;
 }
